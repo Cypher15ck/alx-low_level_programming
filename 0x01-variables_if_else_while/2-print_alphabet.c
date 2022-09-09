e6583ce81e6d71e44332b4bc3c20fd9073e96486
Kingsley15ck <kingsley.cypher1@yahoo.com>
2022-09-09 08:05:24 -0700

    My last commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,31 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 /**
  * main - main block
- * Description: Print statements based on the last digit of the random number.
+ * Description: Use `putchar` function to print the alphabet in lowercase.
  * Return: 0
  */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-	int m;
+	char c = 'a';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	m = n % 10;
-	/* your code goes there */
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
 
-	if (m > 5)
-		printf("Last digit of %i is %i and is greater than 5\n",
-		       n, m);
-	else if (m == 0)
-		printf("Last digit of %i is %i and is 0\n", n, m);
-	else
-		printf("Last digit of %i is %i and is less than 6 and not 0\n",
-		       n, m);
 	return (0);
 }
