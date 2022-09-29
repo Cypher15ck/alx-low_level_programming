68be1fd67a6c84d5d55b8d2212d4f6de57752490
Kingsley15ck <kingsley.cypher1@yahoo.com>
2022-09-28 19:15:22 -0700

    prime number commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,34 @@
-#include <stdio.h>
-
-int is_prime_number(int);
+#include "main.h"
 
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
+ *  * is_prime - detects if an input number is a prime number.
+ *   * @n: input number.
+ *    * @c: iterator.
+ *     * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ *      */
+int is_prime(unsigned int n, unsigned int c)
 {
-int r;
-
-r = is_prime_number(1);
-printf("%d\n", r);
+if (n % c == 0)
+{
+if (n == c)
+return (1);
+else
 return (0);
+}
+return (0 + is_prime(n, c + 1));
+}
+/**
+ *  * is_prime_number - detects if an input number is a prime number.
+ *   * @n: input number.
+ *    * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ *     */
+int is_prime_number(int n)
+{
+if (n == 0)
+return (0);
+if (n < 0)
+return (0);
+if (n == 1)
+return (0);
+return (is_prime(n, 2));
 }
